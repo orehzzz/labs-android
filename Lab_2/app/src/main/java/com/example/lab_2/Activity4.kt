b5968048290f63debb82f21743db960a71e317ae
orehzzz <olegzzz749@gmail.com>
2024-04-05 22:27:47 +0300

    lab_2 final

--- Lab_2/app/src/main/java/com/example/lab_2/Activity4.kt
+++ Lab_2/app/src/main/java/com/example/lab_2/Activity4.kt
@@ -1,8 +1,10 @@
 package com.example.lab_2
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.graphics.Color
 
 class Activity4: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle? ){
@@ -10,5 +12,8 @@ class Activity4: AppCompatActivity() {
         setContentView(R.layout.layout4)
 
         val button: Button = findViewById(R.id.colorButton)
+        button.setOnClickListener{
+            button.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF3DE143")))
+        }
     }
 }
